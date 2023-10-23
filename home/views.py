376137387f89a5cfcc898011cc4e265ce6773ace
bhavyatrivedi01 <bhavyatrivedi1108@gmail.com>
2023-10-22 22:53:25 -0600

    changes now

--- home/views.py
+++ home/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Welcome To DevOps")
+    return HttpResponse("Hello, Let's Learn How DevOps Works!!")
 
