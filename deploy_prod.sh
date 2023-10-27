7aac6c0a57a41fb21d7db341670a4745d983f7b6
bhavyatrivedi01 <36411614+bhavyatrivedi01@users.noreply.github.com>
2023-10-26 21:50:37 -0600

    Update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,6 +1,9 @@
   ssh ubuntu@54.242.149.24 <<EOF
   cd django/myprojectenv/WebApplication
   git pull
+  pip install -r requirements.txt
+  ./manage.py makemigrations
+  ./manage.py migrate --run-syncdb
   sudo service apache2 restart
   exit
 EOF
